9b15aebf81975215879c877a0672d97dbae45c35
Pesiglavets <russdebut8@gmail.com>
2025-10-28 19:40:26 +0300

    added test_ingredient, fixed test_bun test naming

--- tests/test_bun.py
+++ tests/test_bun.py
@@ -5,7 +5,7 @@ from ..bun import Bun
 class TestBun:
 
     @pytest.mark.parametrize('name, price', [('black bun', 100), ('white bun', 200), ('red bun', 300)])
-    def test_bun_creation(self, name, price):
+    def test_bun_creation_valid_params_returns_correct_name_and_price(self, name, price):
         bun = Bun(name, price)
         assert bun.get_name() == name
         assert bun.get_price() == price
---
+++ tests/test_ingredient.py
@@ -0,0 +1,12 @@
+import pytest
+from ..ingredient import Ingredient
+from ..ingredient_types import INGREDIENT_TYPE_SAUCE, INGREDIENT_TYPE_FILLING
+
+
+class TestIngredient:
+    @pytest.mark.parametrize('ingredient_type, name, price', [(INGREDIENT_TYPE_SAUCE, 'hot sauce', 100), (INGREDIENT_TYPE_FILLING, 'cutlet', 100), (INGREDIENT_TYPE_SAUCE, 'chili sauce', 300)])
+    def test_ingredient_creation_correct_data_return_correct_type_name_price(self, ingredient_type, name, price):
+        ingredient = Ingredient(ingredient_type, name, price)
+        assert ingredient.get_type() == ingredient_type
+        assert ingredient.get_name() == name        
+        assert ingredient.get_price() == price
